221374fdda0374375369e6e947d6cc35937b4f7e
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-12-11 13:56:48 +0000

    [2022/12/11/22:56]Toi_Toy

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,28 @@
 Rails.application.routes.draw do
 
-  # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
+  # 管理者側
+  namespace :admin do
+    root to: 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+
+  # 顧客側(scope moduleを用いてURLにpublicが含まれないようにする)
+  scope module: 'public' do
+    root to: 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update, :destroy]
+  end
 
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
+  }
+
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
   }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
