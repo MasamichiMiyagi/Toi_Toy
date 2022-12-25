4e6cd56fc30ff9a729bbf36786ceb2d1a9c754c0
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-12-25 04:17:29 +0000

    [rb-fix]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -16,7 +16,7 @@ class Customer < ApplicationRecord
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/profile_icon.png')
-      profile_image.attach(io: File.open(file_path), filename: 'profile_icon.png', content_type: 'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'profile_icon.png', content_type: 'image/jpg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
