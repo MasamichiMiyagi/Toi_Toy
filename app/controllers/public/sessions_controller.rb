4b30af3cd7adb1c9cc31a8e179b21fc1321d79dc
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2023-02-21 11:18:56 +0000

    [session]render

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -15,7 +15,7 @@ class Public::SessionsController < Devise::SessionsController
       games_path
     else
       #ログイン画面に何も記載しなかった際に、リンクが読み込まれてしまうことがないようにrender先を指定（renderが必要か否か）
-      render new_customer_session_path
+      new_customer_session_path
     end
   end
 
@@ -24,7 +24,7 @@ class Public::SessionsController < Devise::SessionsController
     if super
       root_path
     else
-      render customer_path(current_customer)
+      customer_path(current_customer)
     end
   end
 
