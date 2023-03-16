fc7154944af273bc01028d857b2ef476100bb9c4
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2023-03-16 09:06:11 +0000

    [registrations]public

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  # before_actionは上記2つではなく以下のものを使用
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # GET /resource/sign_up
